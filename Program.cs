4df0d362e70a764713c8fa13ed422d9832b5ba14
chanan-duy <chanan.duy@proton.me>
2025-09-03 15:54:52 +0500

    Test Shapes

--- Program.cs
+++ Program.cs
@@ -1,10 +1,17 @@
-﻿namespace MP_KT5_2025;
+﻿using MP_KT5_2025.Shapes;
+
+namespace MP_KT5_2025;
 
 public static class Program
 {
     private static void TestAnimal(IAnimal animal)
     {
         Console.WriteLine($"{animal.Name} - {animal.MakeSound()}");
+    }
+
+    private static void TestShape(IShape shape)
+    {
+        Console.WriteLine($"area: {shape.Area:F4}\tperimeter: {shape.Perimeter:F4}");
     }
 
     private static void TestAnimals()
@@ -18,6 +25,17 @@ public static class Program
 
     private static void TestShapes()
     {
+        var circle = new Circle(5);
+        Console.WriteLine("circle");
+        TestShape(circle);
+
+        var rectangle = new Rectangle(10, 20);
+        Console.WriteLine("rectangle");
+        TestShape(rectangle);
+
+        var triangle = new Triangle(2, 4, 5);
+        Console.WriteLine("triangle");
+        TestShape(triangle);
     }
 
     private static void TestComparables()
